eaa0710baa28209cfe87215d4b01e730a75aa923
zHandler <zhandler10@gmail.com>
2026-01-01 13:59:24 +0400

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function register() {
   if (data.name == null || data.name == "") return alert("enter name")
   if (data.password == null || data.password == "") return alert("enter password")
 
-  fetch(`${BASE_URL}/auth/register`, {
+  fetch(`${BASE_URL}auth/register`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
@@ -43,7 +43,7 @@ function login() {
   if (data.email == null || data.email == "") return alert("enter email")
   if (data.password == null || data.password == "") return alert("enter password")
 
-  fetch(`${BASE_URL}/auth/login`, {
+  fetch(`${BASE_URL}auth/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
@@ -82,7 +82,7 @@ function checkIn() {
     shift: "AM"
 
   }
-  fetch(`${BASE_URL}/in`, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(checkin) })
+  fetch(`${BASE_URL}in`, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(checkin) })
     .then(res => res.text())
     .then(alert);
 }
@@ -102,14 +102,14 @@ function checkOut() {
     shift: "AM"
 
   }
-  fetch(`${BASE_URL}/in`, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(checkin) })
+  fetch(`${BASE_URL}out`, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(checkin) })
     .then(res => res.text())
     .then(alert);
 }
 
 // Generate Report
 function generateReport() {
-  fetch("http://127.0.0.1:3000/report")
+  fetch(`${BASE_URL}`report")
     .then(res => res.blob())
     .then(blob => {
       const url = window.URL.createObjectURL(blob);
@@ -125,6 +125,7 @@ function generateReport() {
     })
     .catch(err => console.error("Download failed", err));
 }
+
 
 
 
